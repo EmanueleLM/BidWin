af575a3f1aa3c4c2dde6dd980d2ea161288cc3d2
Emanuele <emanuele.lamalfa@mail.polimi.it>
2015-05-28 20:53:45 +0200

    style.css added, minor changes

--- bidwin/src/JavaServerFaces/src/main/java/it/polimi/se2/bidwin/beans/UserBean.java
+++ bidwin/src/JavaServerFaces/src/main/java/it/polimi/se2/bidwin/beans/UserBean.java
@@ -1,3 +1,12 @@
+/*
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+*/
+/* 
+    Created on : 28-mag-2015, 18.24.05
+    Author     : Ga
+*/
 package it.polimi.se2.bidwin.beans;
 
 import javax.faces.bean.ManagedBean;
