b447b73c241647458764443545b5bf7f1120d50e
Emanuele <emanuele.lamalfa@mail.polimi.it>
2015-06-03 16:14:37 +0200

    login commit (a lot of things to do,just a sketch)

---
+++ bidwinRealm/bidwinrealm/src/java/src/ObjectsFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Ga
+ */
+@Stateless
+public class ObjectsFacade extends AbstractFacade<Objects> {
+    @PersistenceContext(unitName = "bidwinrealmPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ObjectsFacade() {
+        super(Objects.class);
+    }
+    
+}
