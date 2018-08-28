c1355f5b65e8718a263c7f00a8a306205cb57ff1
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-08-28 17:54:37 +0200

    no changes

--- script.sh
+++ script.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-
 git filter-branch --env-filter '
 OLD_EMAIL="Mago@Mago-PC"
+CORRECT_NAME="Davide"
+CORRECT_EMAIL="davide.malvestiti@mail.polimi.it"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git filter-branch --env-filter '
+OLD_EMAIL="davide.malvestiti@mail.polimi.it"
 CORRECT_NAME="Davide"
 CORRECT_EMAIL="davide.malvestiti@mail.polimi.it"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
